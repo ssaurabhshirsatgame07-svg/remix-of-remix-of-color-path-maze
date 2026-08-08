911b42c127db440f743b3addc57c5389fea33ce7
Saurabh Shirsat <crashopp2@gmail.com>
2026-08-08 12:33:26 +0530

    Fix production Tailwind build

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import netlify from "@netlify/vite-plugin-tanstack-start";
 import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
     netlify(),
     viteReact(),
+    tailwindcss(),
   ],
+
+  build: {
+    cssMinify: "esbuild",
+  },
 });
